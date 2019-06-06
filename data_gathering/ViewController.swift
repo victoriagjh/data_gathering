0706168b35e61d7c2836e855816bb9dbefc0aa47
victoriagjh <akrso06197@khu.ac.kr>
2019-06-06 15:18:13 +0900

    Initial commit

---
+++ data_gathering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  data_gathering
+//
+//  Created by 권주희 on 06/06/2019.
+//  Copyright © 2019 권주희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
